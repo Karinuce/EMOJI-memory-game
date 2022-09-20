b7e20e32f9c466d942098324ada4c535f1b42ee5
Karinuce <karina.kavaleuskaya@gmail.com>
2022-09-20 15:06:54 +0200

    first commit

---
+++ mcg.js
@@ -0,0 +1,10 @@
+let count = 0;
+let fselect = "";
+let sselect = "";
+
+const cards = document.querySelectorAll(".cards, .card");
+cards.forEach ((card) => {
+    card.addEventListener ("click", () => {
+        console.log (true)
+    });
+});
