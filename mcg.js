347488047c8957d7f2a9e34c34025c59ab7a8197
Karinuce <karina.kavaleuskaya@gmail.com>
2022-09-20 17:15:45 +0200

    my changes

--- mcg.js
+++ mcg.js
@@ -1,10 +1,25 @@
-let count = 0;
 let fselect = "";
 let sselect = "";
+let pressed = 0;
 
-const cards = document.querySelectorAll(".cards, .card");
+const cards = document.querySelectorAll(".all-cards, .cards");
 cards.forEach ((card) => {
-    card.addEventListener ("click", () => {
-        console.log (true)
-    });
+    card.addEventListener ("click", flipCard) => {
+        card.classList.add("pressed");
+
+    if (pressed === 0) {
+        fselect = card.getAttribute("card");
+        pressed++;
+    } else {
+        sselect = card.getAttribute("card");
+        pressed = 0;
+    };
+    if (fselect === sselect) {
+        const pressedCards = card.getAttribute("card" + fselect);
+        pressedCards[0].classList.remove("pressed");
+        pressedCards[1].classList.remove("pressed");
+        pressedCards[0].classList.add("correct");
+        pressedCards[1].classList.add("correct");
+    };
+});
 });
