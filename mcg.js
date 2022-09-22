90b57ef71472a29a3cdd1673459832098ad5d09f
Karinuce <karina.kavaleuskaya@gmail.com>
2022-09-22 17:03:00 +0200

    changes

--- mcg.js
+++ mcg.js
@@ -1,25 +1,51 @@
-let fselect = "";
-let sselect = "";
-let pressed = 0;
+const section = document.querySelector("section");
 
-const cards = document.querySelectorAll(".all-cards, .cards");
-cards.forEach ((card) => {
-    card.addEventListener ("click", flipCard) => {
-        card.classList.add("pressed");
+const getCardsInfo = () => [
+    {imgSrc: "./emoji/glasses.png", name: "glasses"},
+    {imgSrc: "./emoji/heart.png", name: "heart"},
+    {imgSrc: "./emoji/laugh.png", name: "laugh"},
+    {imgSrc: "./emoji/happy.png", name: "happy"},
+    {imgSrc: "./emoji/crying.png", name: "crying"},
+    {imgSrc: "./emoji/confused.png", name: "confused"},
+    {imgSrc: "./emoji/shocked.png", name: "shocked"},
+    {imgSrc: "./emoji/sleepy.png", name: "sleepy"},
+    {imgSrc: "./emoji/glasses.png", name: "glasses"},
+    {imgSrc: "./emoji/heart.png", name: "heart"},
+    {imgSrc: "./emoji/laugh.png", name: "laugh"},
+    {imgSrc: "./emoji/happy.png", name: "happy"},
+    {imgSrc: "./emoji/crying.png", name: "crying"},
+    {imgSrc: "./emoji/confused.png", name: "confused"},
+    {imgSrc: "./emoji/shocked.png", name: "shocked"},
+    {imgSrc: "./emoji/sleepy.png", name: "sleepy"},
+];
 
-    if (pressed === 0) {
-        fselect = card.getAttribute("card");
-        pressed++;
-    } else {
-        sselect = card.getAttribute("card");
-        pressed = 0;
-    };
-    if (fselect === sselect) {
-        const pressedCards = card.getAttribute("card" + fselect);
-        pressedCards[0].classList.remove("pressed");
-        pressedCards[1].classList.remove("pressed");
-        pressedCards[0].classList.add("correct");
-        pressedCards[1].classList.add("correct");
-    };
+const random = () => {
+    const cards = getCardsInfo();
+    cards.sort(() => Math.random() - 0.8);
+    return cards;
+};
+
+const generator = () => {
+    const cards = random();
+    cards.forEach((item) => {
+    const card = document.createElement("div");
+    const frontCard = document.createElement("img");
+    const backCard = document.createElement("div");
+
+    card.addEventListener("click", (e) =>{
+        card.classList.toggle("flippedCard");
+    });
+
+    card.classList = "card";
+    frontCard.classList = "frontCard";
+    backCard.classList = "backCard";
+
+    frontCard.src = item.imgSrc;
+
+    section.appendChild(card);
+    card.appendChild(frontCard);
+    card.appendChild(backCard);
 });
-});
+};
+
+generator();
